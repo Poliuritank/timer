0986cf44daef2d763b900af0d4754b1b453d6cec
Yurii Miroshnychenko <yuriimiro@gmail.com>
2022-09-12 20:31:15 +0300

    fixed time left calculation

--- js/script.js
+++ js/script.js
@@ -1,85 +1,105 @@
-let stopTime
+let stopTime;
 
-const button = document.getElementById('date-picker');
+const button = document.getElementById("date-picker");
 
-const input = document.getElementById('input-date')
-input.value = ''
+const input = document.getElementById("input-date");
+input.value = "";
 
-
-button.addEventListener("click",  () => {
+button.addEventListener("click", () => {
   try {
-     input.showPicker();
+    input.showPicker();
   } catch (error) {
     console.log(error);
   }
 });
 
+input.oninput = () => {
+  if (Date.parse(input.value) > Date.parse(new Date())) {
+    document.querySelector(".input-date-block p").innerHTML = input.value;
 
-input.oninput = ()=>
-{
-    if (Date.parse(input.value) > Date.parse(new Date()))
-    {
-        document.querySelector('.input-date-block p').innerHTML = input.value
-        clearInterval(stopTime)
-        timeFun(input.value)
-    }
-}
+    clearInterval(stopTime);
 
-secondsFun = (total) =>
-    {
-        const seconds = Math.floor((total/1000)%60)
-        const sb = document.getElementById('seconds')
-        sb.querySelector('div ul li:nth-child(1)').innerHTML = ('0'+seconds).slice(-2)
-        let s = 100-((100/60)*seconds)
-        if (seconds===0) s=0
-        sb.style.background = `conic-gradient(#02c2c2 0 ${s}% , #303238 ${s}% 100%)`;
-    }
+    timeFun(input.value);
+  }
+};
 
-minutesFun = (total) =>
-    {
-        const minutes = Math.floor((total/1000/60)%60)
-        const mb = document.getElementById ('minutes')
-        mb.querySelector('div ul li:nth-child(1)').innerHTML = ('0'+minutes).slice(-2)
-        let m = 100-((1000/60)*minutes)
-        if (minutes===0) m=0
-        mb.style.background = `conic-gradient(#02c2c2 0 ${m}% , #303238 ${m}% 100%)`;
-    }
+secondsFun = (total) => {
+  const seconds = Math.floor((total / 1000) % 60);
+  
+  const sb = document.getElementById("seconds");
+  
+  sb.querySelector("div ul li:nth-child(1)").innerHTML = ("0" + seconds).slice(
+    -2
+  );
+  
+  let s = 100 - 100 / 60 * seconds;
+  
+  if (seconds === 0) s = 0;
 
-hoursFun = (total) =>
-    {
-        const hours = Math.floor((total/(1000*60*60))%24)
-        const hb = document.getElementById ('hours')
-        hb.querySelector('div ul li:nth-child(1)').innerHTML = ('0'+hours).slice(-2)
-        let h = 100-((1000/24)*hours)
-        if (hours===0) h=0
-        hb.style.background = `conic-gradient(#02c2c2 0 ${h}% , #303238 ${h}% 100%)`;
-    }
+  sb.style.background = `conic-gradient(#02c2c2 0 ${s}% , #303238 ${s}% 100%)`;
+};
 
-daysFun = (total) =>
-    {
-        const days = Math.floor(total/(1000*60*60*24))
-        const db = document.getElementById('days')
-        db.querySelector('div ul li:nth-child( 1 )' ).innerHTML = ('0'+ days).slice( -2 )
-        let d = 100 / days
-        if (days===0) d=0
-        db.style.background = `conic-gradient(#02c2c2 0 ${d}% , #303238 ${d}% 100%)`;
-    }
+minutesFun = (total) => {
+  const minutes = Math.floor((total / 1000 / 60) % 60);
 
+  const mb = document.getElementById("minutes");
+  
+  mb.querySelector("div ul li:nth-child(1)").innerHTML = ("0" + minutes).slice(
+    -2
+  );
+  
+  let m = 100 - 100 / 60 * minutes;
+  
+  if (minutes === 0) m = 0;
+  
+  mb.style.background = `conic-gradient(#02c2c2 0 ${m}% , #303238 ${m}% 100%)`;
+};
 
-timeFun = endtime =>
-{
-    intervalFun = () =>
-    {
-        const total = Date.parse(endtime) - Date.parse (new Date())
+hoursFun = (total) => {
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  
+  const hb = document.getElementById("hours");
+  
+  hb.querySelector("div ul li:nth-child(1)").innerHTML = ("0" + hours).slice(
+    -2
+  );
+  
+  let h = 100 - 100 / 24 * hours;
+  
+  if (hours === 0) h = 0;
+  
+  hb.style.background = `conic-gradient(#02c2c2 0 ${h}% , #303238 ${h}% 100%)`;
+};
 
-        secondsFun(total)
-        minutesFun(total)
-        hoursFun(total)
-        daysFun(total)
+daysFun = (total) => {
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  
+  const db = document.getElementById("days");
+  
+  db.querySelector("div ul li:nth-child( 1 )").innerHTML = ("0" + days).slice(
+    -2
+  );
+  
+  let d = 100 / days;
+  
+  if (days === 0) d = 0;
+  
+  db.style.background = `conic-gradient(#02c2c2 0 ${d}% , #303238 ${d}% 100%)`;
+};
 
-        if (total <=0) clearInterval (stopTime)
-    }
+timeFun = (endtime) => {
+  const intervalFun = () => {
+    const total = Date.parse(endtime) - Date.parse(new Date());
 
-    intervalFun ()
-    stopTime = setInterval(intervalFun , 1000)
-}
+    secondsFun(total);
+    minutesFun(total);
+    hoursFun(total);
+    daysFun(total);
+
+    if (total <= 0) clearInterval(stopTime);
+  };
+
+  intervalFun();
+
+  stopTime = setInterval(intervalFun, 1000);
+};
